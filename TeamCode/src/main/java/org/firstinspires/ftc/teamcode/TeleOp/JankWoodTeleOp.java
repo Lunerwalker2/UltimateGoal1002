1ce50b8588215684f2dc3eeffcb6e03ed13bb74d
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2021-03-04 20:07:41 -0500

    feat- Adjusted wood bot tele for dt test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/JankWoodTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/JankWoodTeleOp.java
@@ -23,31 +23,31 @@ public class JankWoodTeleOp extends LinearOpMode {
     static final double VY_WEIGHT = 1;
     static final double OMEGA_WEIGHT = 1;
 
-    static final double goalClawOpen = 0.8;
-    static final double goalClawClose = 0.4;
-
-    static final double ringClawOpen = 0.0;
-    static final double ringClawClose = 0.6;
+//    static final double goalClawOpen = 0.8;
+//    static final double goalClawClose = 0.4;
+//
+//    static final double ringClawOpen = 0.0;
+//    static final double ringClawClose = 0.6;
 
     DcMotorEx lf;
     DcMotorEx lb;
     DcMotorEx rf;
     DcMotorEx rb;
 
-    DcMotorEx goalArm;
-    DcMotorEx ringArm;
-
-    Servo goalClaw;
-    Servo ringClaw;
+//    DcMotorEx goalArm;
+//    DcMotorEx ringArm;
+//
+//    Servo goalClaw;
+//    Servo ringClaw;
 
     private double slowModeMult = 0.7;
 
 
-    private boolean ringClawBounce = false;
-    private boolean ringClawChanged = false;
-
-    private boolean goalClawChanged = false;
-    private boolean goalClawBounce = false;
+//    private boolean ringClawBounce = false;
+//    private boolean ringClawChanged = false;
+//
+//    private boolean goalClawChanged = false;
+//    private boolean goalClawBounce = false;
 
     ArrayList<DcMotorEx> motors = new ArrayList<>();
 
@@ -61,11 +61,11 @@ public class JankWoodTeleOp extends LinearOpMode {
         rf = hardwareMap.get(DcMotorEx.class, "rf");
         rb = hardwareMap.get(DcMotorEx.class, "rb");
 
-        goalArm = hardwareMap.get(DcMotorEx.class, "goal arm");
-        ringArm = hardwareMap.get(DcMotorEx.class, "ring arm");
-
-        goalClaw = hardwareMap.get(Servo.class, "goal claw");
-        ringClaw = hardwareMap.get(Servo.class, "ring claw");
+//        goalArm = hardwareMap.get(DcMotorEx.class, "goal arm");
+//        ringArm = hardwareMap.get(DcMotorEx.class, "ring arm");
+//
+//        goalClaw = hardwareMap.get(Servo.class, "goal claw");
+//        ringClaw = hardwareMap.get(Servo.class, "ring claw");
 
         motors.add(lf);
         motors.add(lb);
@@ -85,52 +85,52 @@ public class JankWoodTeleOp extends LinearOpMode {
         rb.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
-        goalArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        goalArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        goalArm.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        ringArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        ringArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        goalArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        goalArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        goalArm.setDirection(DcMotorSimple.Direction.REVERSE);
+//
+//        ringArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        ringArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
         waitForStart();
 
-        goalClaw.setPosition(goalClawOpen);
-        ringClaw.setPosition(ringClawOpen);
+//        goalClaw.setPosition(goalClawOpen);
+//        ringClaw.setPosition(ringClawOpen);
 
 
         while (opModeIsActive()){
             if (isStopRequested()) return;
-
-            if(gamepad2.a && !goalClawBounce){
-                moveGoalClaw();
-                goalClawBounce = true;
-            } else if(!gamepad2.a){
-                goalClawBounce = false;
-            }
-
-
-            if(gamepad2.left_bumper && !ringClawBounce){
-                moveRingClaw();
-                ringClawBounce = true;
-            } else if(!gamepad2.left_bumper){
-                ringClawBounce = false;
-            }
-
-
-            if(gamepad2.dpad_up) ringArm.setPower(0.8);
-            else if(gamepad2.dpad_down) ringArm.setPower(-0.8);
-            else ringArm.setPower(0);
-
-            if(gamepad2.left_trigger >= 0.2) goalArm.setPower(0.4);
-            else if(gamepad2.right_trigger >= 0.2) goalArm.setPower(-0.2);
-            else goalArm.setPower(0);
-
-            //We use RoadRunner's reverse kinematics for wheel powers
-            /* It's some weird voodoo magic that somehow works.
-            It requires us to reverse the previous gamepad values in ways that are frankly weird.
-            But it does math for us so we do it :)
-             */
+//
+//            if(gamepad2.a && !goalClawBounce){
+//                moveGoalClaw();
+//                goalClawBounce = true;
+//            } else if(!gamepad2.a){
+//                goalClawBounce = false;
+//            }
+//
+//
+//            if(gamepad2.left_bumper && !ringClawBounce){
+//                moveRingClaw();
+//                ringClawBounce = true;
+//            } else if(!gamepad2.left_bumper){
+//                ringClawBounce = false;
+//            }
+//
+//
+//            if(gamepad2.dpad_up) ringArm.setPower(0.8);
+//            else if(gamepad2.dpad_down) ringArm.setPower(-0.8);
+//            else ringArm.setPower(0);
+//
+//            if(gamepad2.left_trigger >= 0.2) goalArm.setPower(0.4);
+//            else if(gamepad2.right_trigger >= 0.2) goalArm.setPower(-0.2);
+//            else goalArm.setPower(0);
+//
+//            //We use RoadRunner's reverse kinematics for wheel powers
+//            /* It's some weird voodoo magic that somehow works.
+//            It requires us to reverse the previous gamepad values in ways that are frankly weird.
+//            But it does math for us so we do it :)
+//             */
 
             Pose2d roadRunnerVoodooPowers = new Pose2d(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
             List<Double> drivePowers = getDrivePowers(normalizedVels(roadRunnerVoodooPowers));
@@ -146,31 +146,31 @@ public class JankWoodTeleOp extends LinearOpMode {
         }
 
         motors.forEach((motor) -> motor.setPower(0));
-        goalArm.setPower(0);
+//        goalArm.setPower(0);
 
 
 
     }
 
-    private void moveGoalClaw(){
-        if(!goalClawChanged){
-            goalClaw.setPosition(goalClawOpen);
-            goalClawChanged = true;
-        } else {
-            goalClaw.setPosition(goalClawClose);
-            goalClawChanged = false;
-        }
-    }
-
-    private void moveRingClaw(){
-        if(!ringClawChanged){
-            ringClaw.setPosition(ringClawOpen);
-            ringClawChanged = true;
-        } else {
-            ringClaw.setPosition(ringClawClose);
-            ringClawChanged = false;
-        }
-    }
+//    private void moveGoalClaw(){
+//        if(!goalClawChanged){
+//            goalClaw.setPosition(goalClawOpen);
+//            goalClawChanged = true;
+//        } else {
+//            goalClaw.setPosition(goalClawClose);
+//            goalClawChanged = false;
+//        }
+//    }
+//
+//    private void moveRingClaw(){
+//        if(!ringClawChanged){
+//            ringClaw.setPosition(ringClawOpen);
+//            ringClawChanged = true;
+//        } else {
+//            ringClaw.setPosition(ringClawClose);
+//            ringClawChanged = false;
+//        }
+//    }
 
     public static List<Double> getDrivePowers(Pose2d drivePower){
         return MecanumKinematics.robotToWheelVelocities(drivePower,
