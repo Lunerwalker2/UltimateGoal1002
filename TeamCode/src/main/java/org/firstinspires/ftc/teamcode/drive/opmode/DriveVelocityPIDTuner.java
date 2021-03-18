8afe38995fbfb04f21263979a1667c574f850c79
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2021-03-17 21:17:25 -0400

    feat: PID tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DriveVelocityPIDTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DriveVelocityPIDTuner.java
@@ -148,7 +148,7 @@ public class DriveVelocityPIDTuner extends LinearOpMode {
                             new Pose2d(
                                     -gamepad1.left_stick_y,
                                     -gamepad1.left_stick_x,
-                                    -gamepad1.right_stick_x
+                                    gamepad1.right_stick_x
                             )
                     );
                     break;
